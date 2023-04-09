5e899d461aa8c03eb880f2f77c3303e5b58a2ee4
Інга Мостова <ingo4ka20041997@gmail.com>
2023-04-09 21:39:38 +0300

    done task

--- models/contacts.js
+++ models/contacts.js
@@ -1,14 +1,71 @@
 // const fs = require('fs/promises')
 
-const listContacts = async () => {}
+// const listContacts = async () => {}
 
-const getContactById = async (contactId) => {}
+// const getContactById = async (contactId) => {}
 
-const removeContact = async (contactId) => {}
+// const removeContact = async (contactId) => {}
 
-const addContact = async (body) => {}
+// const addContact = async (body) => {}
 
-const updateContact = async (contactId, body) => {}
+const fs = require("fs").promises;
+const path = require("path");
+const { nanoid } = require("nanoid");
+
+const contactsPath = path.join(__dirname, "contacts.json");
+
+async function readContacts() {
+  const data = await fs.readFile(contactsPath, "utf8");
+  return JSON.parse(data);
+}
+
+function updateContacts(contacts) {
+  return fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2), "utf-8");
+}
+
+function listContacts() {
+  return readContacts();
+}
+
+async function getContactById(contactId) {
+  const contacts = await readContacts();
+  const contact = contacts.find((contact) => contact.id === contactId);
+
+  return contact || null;
+}
+
+async function removeContact(contactId) {
+  const contacts = await readContacts();
+  const idx = contacts.findIndex((item) => item.id === contactId);
+  if (idx === -1) {
+    return null;
+  }
+  const result = contacts.splice(idx, 1);
+  await updateContacts(contacts);
+  return result;
+}
+
+async function addContact(contact) {
+  const contacts = await readContacts();
+  const newContact = { id: nanoid(), ...contact };
+
+  contacts.push(newContact);
+
+  await updateContacts(contacts);
+
+  return newContact;
+}
+
+const updateContact = async (contactId, data) => {
+  const contacts = await readContacts();
+  const idx = contacts.findIndex((item) => item.id === contactId);
+  if (idx === -1) {
+    return null;
+  }
+  contacts[idx] = { contactId, ...data };
+  await updateContacts(contacts);
+  return contacts[idx];
+};
 
 module.exports = {
   listContacts,
@@ -16,4 +73,4 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
-}
+};
